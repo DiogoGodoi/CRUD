477064998b3a5730d46e6f3a62cf06e87e385f94
Diogo <diogogoddoi@gmail.com>
2023-04-10 15:38:00 -0300

    Atualização

--- Control/ctrlPessoas.cs
+++ Control/ctrlPessoas.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Model;
 
-namespace CRUD
+namespace Control
 {
 	public class ctrlPessoas
 	{
--- Model/mdlPessoas.cs
+++ Model/mdlPessoas.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CRUD
+namespace Model
 {
 	public class mdlPessoas
 	{
